201b9cf2a0d06572afb64a8e01b077304c42ad39
mrkeyboard <matthewdandar@gmail.com>
2022-11-27 21:49:32 -0800

    configured deploy to use compose

--- receiver/app.py
+++ receiver/app.py
@@ -121,4 +121,4 @@ app = connexion.FlaskApp(__name__, specification_dir='', options=options)
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True) 
 
 if __name__ == "__main__":
-    app.run(port=8070)
+    app.run(port=8080)
