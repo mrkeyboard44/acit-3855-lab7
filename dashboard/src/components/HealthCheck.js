264d7c90571a8e0457ab1a0379f2baffef8d0ad9
mrkeyboard <matthewdandar@gmail.com>
2022-12-01 14:59:09 -0800

    assi 2

--- dashboard/src/components/HealthCheck.js
+++ dashboard/src/components/HealthCheck.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import '../App.css';
+import { Stack } from "@mui/material";
 
 export default function EndpointHealth(props) {
     const [isLoaded, setIsLoaded] = useState(false);
-    const [log, setLog] = useState(null);
+    const [health, setHealth] = useState(null);
     const [error, setError] = useState(null);
 	// const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
     const getHealth = () => {
@@ -11,7 +12,7 @@ export default function EndpointHealth(props) {
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Health Results")
-                setLog(result);
+                setHealth(result);
                 setIsLoaded(true);
             },(error) =>{
                 setError(error)
@@ -28,10 +29,46 @@ export default function EndpointHealth(props) {
     } else if (isLoaded === false){
         return(<div>Loading...</div>)
     } else if (isLoaded === true){
-        
+        const color = (status) => {
+            if (status == 'Running') {
+                return 'statusGreen'
+            } else {
+                return 'statusRed'
+            }
+        }
+
+        const [date, time_with_sec] = health['last_update'].split('T')
+        const [time, _ ] = time_with_sec.split('.')
+        let last_update = String(date) + ' ' + String(time)
+
         return (
+
             <div className='Health'>
-                <p id='Health'>{JSON.stringify(log).replaceAll('{','{\t').replaceAll(',', ',\n\t').replaceAll('}', '\t}')}</p>
+                <Stack direction={'row'} justifyContent={'space-evenly'} spacing={2}>
+                    <Stack>
+                        <h4>Reciever Status:</h4>
+                        <p className={color(health['receiver'])}>{health['receiver']}</p>
+
+                    </Stack>
+                    <Stack>
+                        <h4>Storage Status:</h4>
+                        <p className={color(health['storage'])}>{health['storage']}</p>
+
+                    </Stack>
+                    <Stack>
+                        <h4>Processing Status:</h4>
+                        <p className={color(health['processing'])}>{health['processing']}</p>
+                        
+                    </Stack>
+                    <Stack>
+                        <h4>Audit Log Status:</h4>
+                        <p className={color(health['audit_log'])}>{health['audit_log']}</p>
+                        
+                    </Stack>
+                </Stack>
+
+						
+                <h3>Last Updated: {last_update}</h3>
             </div>
         )
     }
